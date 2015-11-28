e9191a3234cd309b694691c59dfac3bccaeba685
Brent J Sullivan <brntsllvn@gmail.com>
2015-11-28 03:33:45 +0000

    script debug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,16 @@ class WelcomeController < ApplicationController
 
   def index
     agent = Mechanize.new
-    @page = agent.get('https://www.wealthfront.com/login')
+    page = agent.get('https://www.wealthfront.com/login')
+    login_form = page.forms.first
+
+    login_form.email    = ENV["WF_EMAIL"] 
+    login_form.password = ENV["WF_PASSWORD"]
+
+    home_page = login_form.submit
+
+    @login = page.title
+    @home  = home_page.title
   end
-  
+
 end
