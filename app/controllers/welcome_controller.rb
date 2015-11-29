e05f39a2509de32ecc3ae298d632064d9f64377c
Brent J Sullivan <brntsllvn@gmail.com>
2015-11-29 01:14:35 +0000

    scrape wf

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,6 +12,27 @@ class WelcomeController < ApplicationController
 
     @login = page.title
     @home  = home_page.title
+
+    @portfolio_title = home_page.search(".current-title").text
+    @account_value = home_page.search(".ofa-total-and-return > h2").text
+    @return = home_page.search(".dash-sidebar-subinfo").text
+
+    scraped_asset_classes     = home_page.search("td > span > a.ktip-a").map(&:text).map(&:strip)
+    wealthfront_asset_classes = ["U.S. Stocks", "Foreign Stocks", "Emerging Markets", 
+      "Dividend Stocks", "Natural Resources", "Municipal Bonds", "TIPS"]
+    @ast_cls = ( scraped_asset_classes & wealthfront_asset_classes ) + ["Cash"]
+    @pct     = home_page.search("td.pct").map(&:text).map(&:strip)
+    @mkt_val = home_page.search("td.value").map(&:text).map(&:strip)
+    @basis   = home_page.search("td.basis").map(&:text).map(&:strip)
+    @gain    = home_page.search("td.gain").map(&:text).map(&:strip)
+
+    transactions_page = home_page.link_with(href: "/accounts/60823/transactions").click
+
+    @trans_date = transactions_page.search(".historical_transaction-row > td.date").map(&:text).map(&:strip)
+    @trans_act  = transactions_page.search(".historical_transaction-row > td.activity").map(&:text).map(&:strip)
+    @trans_amt  = transactions_page.search(".historical_transaction-row > td.numeric.amount").map(&:text).map(&:strip)
+
+
   end
 
 end
